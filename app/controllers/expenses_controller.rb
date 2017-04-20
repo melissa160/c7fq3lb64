0af9bd77f002862e01de252ec0d8c99fc49ff854
melissa160 <melissa160@gmail.com>
2017-04-20 14:12:11 -0500

    Adicionando funcionalidad de busqueda

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,12 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+    @expenses = Expense.order("date DESC")
+    if params[:concept].present? && params[:category_id].present?
+      @expenses = @expenses.where("concept LIKE ? AND category_id = ?", "%#{params[:concept]}%", params[:category_id])
+    elsif params[:concept].present?
+      @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
+    elsif params[:category_id].present?
+      @expenses = @expenses.where("category_id = ?", params[:category_id])
+    end
   end
 end
