e9f739950fce88793e3628e703152d1a70c909cd
melissa160 <melissa160@gmail.com>
2017-04-20 18:17:03 -0500

    implementar usuarios para los gastos

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,11 +1,13 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    @expenses = Expense.order("date DESC")
-    if params[:concept].present? && params[:category_id].present?
-      @expenses = @expenses.where("concept LIKE ? AND category_id = ?", "%#{params[:concept]}%", params[:category_id])
-    elsif params[:concept].present?
+    # @expenses = Expense.order("date DESC")
+    @expenses = current_user.expenses.all
+    if params[:concept].present?
       @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
-    elsif params[:category_id].present?
+    end
+    if params[:category_id].present?
       @expenses = @expenses.where("category_id = ?", params[:category_id])
     end
   end
